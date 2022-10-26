a5c2b9a50b4141e56912f6c94bb261c60940442d
martins <gabrielamspinola@gmail.com>
2022-10-26 02:25:35 -0300

    tentativa de correção

--- src/Monster.ts
+++ src/Monster.ts
@@ -18,9 +18,12 @@ export default class Monster implements SimpleFighter {
   }
 
   receiveDamage(attackPoints: number): number {
-    if (attackPoints > 0) this._lifePoints -= attackPoints;
-    if (this._lifePoints <= 0) this._lifePoints = -1;
-
+    if (attackPoints > 0) {
+      this._lifePoints -= attackPoints;
+    }
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
     return this._lifePoints;
   }
 
